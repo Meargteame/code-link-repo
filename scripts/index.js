3eef144644053236a186ed12f74a8e06b2a243ec
Meargteame <mearegteame99995555@gmail.com>
2024-12-11 22:56:36 +0300

    Workshops page added

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+// JavaScript to handle button click and redirect
+document.getElementById('signUpBtn').addEventListener('click', function() {
+    window.location.href = 'https://t.me/+GICpHcxMKwY1YjM0'; // Replace with your actual Telegram group link
+  });
